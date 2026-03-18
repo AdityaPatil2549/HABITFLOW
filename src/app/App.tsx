e496eab75f2317dac343481482076b34e4b56b51
AdityaPatil2549 <adityapatil2549@gmail.com>
2026-03-18 23:06:42 +0530

    Remove sample data seeding from app startup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,11 @@
-import { Suspense, useEffect } from 'react'
+import { Suspense } from 'react'
 import { Router } from './Router'
 import { ThemeProvider } from './providers/ThemeProvider'
 import { QueryProvider } from './providers/QueryProvider'
 import { ErrorBoundary } from '@/shared/components/ErrorBoundary'
 import { PageSkeleton } from '@/shared/components/PageSkeleton'
-import { seedDatabase } from '@/db/seed'
 
 export const App: React.FC = () => {
-  useEffect(() => {
-    // Seed database with sample data on first load
-    seedDatabase().catch(console.error)
-  }, [])
-
   return (
     <ErrorBoundary>
       <ThemeProvider>
